0f8ac0035c5cdfd164ce492dfdf5c7dfa0963955
mrkaptep <kaptep@yahoo.com>
2022-04-27 16:44:48 -0600

    fix2

--- src/sharedUtils/validationUtils.js
+++ src/sharedUtils/validationUtils.js
@@ -10,7 +10,7 @@ let SignupSchema = yup.object().shape({
       .string()
       .required("This field is required.")
       .matches(
-         /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+         /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{12,}$/,
          "Must Contain 12 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
       ),
    ConfirmPASSWD: yup

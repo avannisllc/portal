0f7c8085eaf117b321f2bf8ac98316138cda9f86
mrkaptep <kaptep@yahoo.com>
2022-04-27 17:18:26 -0600

    yup2

--- src/sharedUtils/validationUtils.js
+++ src/sharedUtils/validationUtils.js
@@ -6,15 +6,12 @@ let SignupSchema = yup.object().shape({
    CONTACT_FNAME: yup.string().required("This field is required."),
    CONTACT_LNAME: yup.string().required("This field is required."),
    USERNAME: yup.string().required("This field is required."),
-   PASSWD: yup
-      .string()
+   PASSWD: yup.string()
       .required("This field is required.")
-      .matches(
-         /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{12,}$/,
+      .matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{12,}$/,
          "Must Contain 12 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
       ),
-   ConfirmPASSWD: yup
-      .string()
+   ConfirmPASSWD: yup.string()
       .oneOf([yup.ref('PASSWD'), null], 'Passwords must match')
       .required('Confirm Password is required'),
    EMAIL: yup
@@ -28,29 +25,23 @@ let UpdateUserSchema = yup.object().shape({
    CONTACT_FNAME: yup.string().required("This field is required."),
    CONTACT_LNAME: yup.string().required("This field is required."),
    USERNAME: yup.string().required("This field is required."),
-   PASSWD: yup
-      .string()
+   PASSWD: yup.string()
       .required("This field is required.")
-      .matches(
-         /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{12,}$/,
+      .matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{12,}$/,
          "Must Contain 12 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
       ),
-   EMAIL: yup
-      .string()
+   EMAIL: yup.string()
       .email("Invalid email")
       .required("This field is required."),
 });
 
 let ResetPWSchema = yup.object().shape({
-   PASSWD: yup
-      .string()
+   PASSWD: yup.string()
       .required("This field is required.")
-      .matches(
-         /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{12,}$/,
+      .matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{12,}$/,
          "Must Contain 12 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
       ),
-   ConfirmPASSWD: yup
-      .string()
+   ConfirmPASSWD: yup.string()
       .oneOf([yup.ref('PASSWD'), null], 'Passwords must match')
       .required('Confirm Password is required'),
 });
